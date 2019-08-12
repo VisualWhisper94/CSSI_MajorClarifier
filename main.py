b76ef9f0f0b1dafdf4276395d9967b5ba3ea2ee4
VisualWhisper94 <codyramsey@mec.science>
2019-08-12 16:02:34 -0400

    4th push for the day

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import webapp2
 import os
 import jinja2
-import random
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MajorHandler(webapp2.RequestHandler):
+    """docstring for ."""
+    def get(self):
+        t = the_jinja_env.get_template('templates/major.html')
+        self.response.write(t.render())
+
+routes = [('/', MajorHandler),]
+app = webapp2.WSGIApplication(routes ,debug=True)
