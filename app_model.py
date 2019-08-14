2b187e50072b71e182e241cce82d07a9eabfd22b
samueljames <samueljames@mec.science>
2019-08-14 10:48:15 -0400

    hello

--- app_model.py
+++ app_model.py
@@ -1,8 +1,12 @@
-"""from google.appengine.ext import ndb
+from google.appengine.ext import ndb
 
 class Class (ndb.Model):
     name= ndb.StringProperty(required= True)
     gpa = ndb.FloatProperty (requiered= True, defualt=0.00)
 
 ANCESTORY_KEY= ndb.Key("Class","startkey")
-gpa= """
+gpa= 
+
+
+
+if 
