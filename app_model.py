d9f4b3c6302ab80f1e1b811814881d94aeaad549
VisualWhisper94 <codyramsey@mec.science>
2019-08-14 11:00:45 -0400

    Cody's First commit today

--- app_model.py
+++ app_model.py
@@ -1,8 +1,7 @@
-"""from google.appengine.ext import ndb
+from google.appengine.ext import ndb
 
 class Class (ndb.Model):
     name= ndb.StringProperty(required= True)
     gpa = ndb.FloatProperty (requiered= True, defualt=0.00)
 
 ANCESTORY_KEY= ndb.Key("Class","startkey")
-gpa= """
